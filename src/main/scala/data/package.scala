78974310419fe9d19753ec811c3b50ceb9cbaddb
LinuzJ <linusjern00@gmail.com>
2021-03-16 15:56:06 +0200

    Created data package to clear up the main file

---
+++ src/main/scala/data/package.scala
@@ -0,0 +1,15 @@
+package data
+import io.circe.Json
+import io.circe.parser.parse
+import scalaj.http.{Http, HttpResponse}
+
+class Datak {
+  val response: HttpResponse[String] = Http("https://query1.finance.yahoo.com/v8/finance/chart/AAPL?interval=5m").asString
+
+    val json: Json = parse(response.body).getOrElse(Json.Null)
+
+    println(response.body)
+
+    def getData = json
+    def getResonse = response.body
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,43 +2,22 @@ import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.control.Label
 import scalafx.scene.layout.Pane
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
-//import scala.util.parsing.json._
-
-import scalaj.http._
-
-
+import data.Datak
 
 object Main extends JFXApp {
-    /*
-  Creation of a new primary stage (Application window).
-  We can use Scala's anonymous subclass syntax to get quite
-  readable code.
-  */
-  val response: HttpResponse[String] = Http("https://query1.finance.yahoo.com/v8/finance/chart/AAPL?interval=5m").asString
-
-//  val parseResult = parse(response.body).getOrElse(Json.Null)
-//  val cursor = parseResult.hcursor
-//
-//
-//
-//  println(parseResult)
-
 
   stage = new JFXApp.PrimaryStage {
       title.value = "Cool ass stonks graph"
-      width = 600
-      height = 450
+      width = 800
+      height = 650
   }
-
-
+  val a = new Datak
 
   val root = new Pane //Simple pane component
   val scene = new Scene(root) //Scene acts as a container for the scene graph
   stage.scene = scene
-
-  root.children = new Label(response.body)
-
+  println(a.getResonse)
+  root.children = new Label("hej")
 
 }
 
