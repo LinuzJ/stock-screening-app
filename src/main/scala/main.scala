174fc79c9e4b01187d1a156c8a4612d584dfff23
LinuzJ <linusjern00@gmail.com>
2021-04-08 16:29:12 +0300

    Changed the Unix timestamp to Datetime in the graph. Right now the implementation of this is npt finished, but works

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,8 +4,9 @@ import scalafx.scene.control.Label
 import scalafx.scene.layout.Pane
 import data.Data
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.chart.{NumberAxis, ScatterChart, XYChart}
+import scalafx.scene.chart.{CategoryAxis, NumberAxis, ScatterChart, XYChart}
 
+import java.text.SimpleDateFormat
 import java.time.Instant
 
 object Main extends JFXApp {
@@ -13,8 +14,9 @@ object Main extends JFXApp {
 
 
   // timeframe for graph (days)
-  val time: Int = 4
+  val time: Int = 1
   val unixTime: Long = Instant.now.getEpochSecond - (time * 24*60*60)
+  val timeFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss")
 
   // interval for graph (minutes)
   val interval = 5
@@ -24,21 +26,24 @@ object Main extends JFXApp {
   val data2 = new Data(s"https://query1.finance.yahoo.com/v8/finance/chart/NOK?symbol=AAPL&period1=${unixTime}&period2=9999999999&interval=${interval}m")
   val data3 = new Data(s"https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=BABA&period1=${unixTime}&period2=9999999999&interval=${interval}m")
 
-  val X                 = data1.getPriceData.map(_.head)
+  var X                 = data1.getPriceData.map(_.head)
   val Y                 = data1.getPriceData.map(_(1))
-  val wrapper           = X zip Y
+  val X_head            = X.head
+  val X_formatted = X.map(_ - X_head).map(timeFormat.format(_))
+
+  val wrapper           = X_formatted zip Y
 
   stage = new JFXApp.PrimaryStage {
       title.value = "Data dashboard"
       scene = new Scene(700, 700) {
-        val xAxis = NumberAxis()
+        val xAxis = CategoryAxis()
         val yAxis = NumberAxis()
 
-        val pData = XYChart.Series[Number, Number](
+        val Data = XYChart.Series[String, Number](
           "Data",
-          ObservableBuffer(wrapper.map(z => XYChart.Data[Number, Number](z._1, z._2)): _*))
-        val plot = new ScatterChart(xAxis, yAxis, ObservableBuffer(pData))
-
+          ObservableBuffer(wrapper.map(i => XYChart.Data[String, Number](i._1, i._2)): _*))
+        val plot = new ScatterChart(xAxis, yAxis)
+        plot.getData().add(Data)
         root     = plot
       }
   }
