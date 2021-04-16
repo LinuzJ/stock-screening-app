8b6fe2cff09256d759df0a8c1f94a370061c46ef
LinuzJ <linusjern00@gmail.com>
2021-04-16 14:18:45 +0300

    Changed to BorderPane/SplitPane setup

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,7 @@
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, Label}
-import scalafx.scene.layout.{GridPane, Pane}
+import scalafx.scene.control.{Button, Label, SplitPane}
+import scalafx.scene.layout.{BorderPane, GridPane, Pane}
 import data.Data
 import charts.Line
 import charts.Pie
@@ -27,23 +27,28 @@ object Main extends JFXApp {
   val time: Int = 10
   // interval for graph (minutes) Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
   val interval = 30
+
+
   // here you choose which stocks to monitor
   var stocks: Seq[String] = List("AAPL", "GME", "TSLA")
+
+
+
   // helper method for changing the stocks
   def changeStocks(input: Seq[String]): Unit = { stocks = input }
 
   // The fetched data is stored here
   val unixTime: Long = Instant.now.getEpochSecond - (time * 24*60*60)
   // method for getting instances of Data for the different stocks
-  def data(inputStocks: Seq[String]): Seq[(String, Data)] = inputStocks.map{
+  def buildData(inputStocks: Seq[String]): Seq[(String, Data)] = inputStocks.map{
     x =>
     (x, new Data(s"https://query1.finance.yahoo.com/v8/finance/chart/${x}?symbol=${x}&period1=${this.unixTime}&period2=9999999999&interval=${this.interval}m"))
   }
 
   // charts
-  val lineChart     = new Line(data(stocks))
-  val pieChart      = new Pie(data(stocks))
-  val barChart      = new Bar(data(stocks))
+  val lineChart     = new Line(buildData(stocks))
+  val pieChart      = new Pie(buildData(stocks))
+  val barChart      = new Bar(buildData(stocks))
   val listOfCharts: Seq[charts.Chart] = List(lineChart, pieChart, barChart)
 
 
@@ -53,20 +58,36 @@ object Main extends JFXApp {
     // FOT THE SAKE OF THE TEST CHANGE THE STOCKS TO THIS
     changeStocks(List("AAPL", "GME", "NOK"))
     for (chart <- listOfCharts) {
-      chart.update(data(stocks))
+      chart.update(buildData(stocks))
     }
     updateStage()
   }
+
+
   def newStage: JFXApp.PrimaryStage = {
     new JFXApp.PrimaryStage {
 
       title.value = "Data dashboard"
-      val pane = new GridPane
+      val pane = new BorderPane()
+      val splitLeft = new SplitPane()
+      splitLeft.items.add(lineChart.getPane)
+      splitLeft.items.add(barChart.getPane)
+      splitLeft.orientation = scalafx.geometry.Orientation.Vertical
+      val splitRight = new SplitPane()
+      splitRight.items.add(pieChart.getPane)
+      splitRight.items.add(button)
+      splitRight.orientation = scalafx.geometry.Orientation.Vertical
 
-      pane.add(lineChart.getPane, 0, 0)
-      pane.add(pieChart.getPane, 1, 0)
-      pane.add(barChart.getPane, 0, 1)
-      pane.add(button, 1, 1)
+      val splitCenter = new SplitPane()
+      splitCenter.items.add(splitLeft)
+      splitCenter.items.add(splitRight)
+      splitCenter.orientation = scalafx.geometry.Orientation.Horizontal
+
+      pane.setCenter(splitCenter)
+//      pane.add(lineChart.getPane, 0, 0)
+//      pane.add(pieChart.getPane, 1, 0)
+//      pane.add(barChart.getPane, 0, 1)
+//      pane.add(button, 1, 1)
       scene = new Scene(pane, 1000, 1000)
 
     }
