cb2627d7111d81ec5471343941a27f1b4f5a55a8
LinuzJ <linusjern00@gmail.com>
2021-04-19 00:20:15 +0300

    cleanup

--- src/main/scala/data/readIO.scala
+++ src/main/scala/data/readIO.scala
@@ -7,24 +7,12 @@ class readTickers {
    def readMyFile(sourceFile: String): Seq[(String, String)] = {
      var seqOfTickers: Seq[(String, String)] = List()
      try {
-        // opens an incoming character stream from the file
         val fileIn = new FileReader(sourceFile)
-
-        // Creates a buffered stream with which it is possible to
-        // read line by line from the previous stream
 
         val linesIn = new BufferedReader(fileIn)
 
-        // At this point, the streams should be open
-        // so we must remember to close them.
         try {
-
-          // Read the text from the stream line by line until the read line
-          // is null. Then we know that
-          // the stream (and also the file) end has been reached.
-
             var oneLine = linesIn.readLine()
-
             while (oneLine != null) {
                 val split: Seq[String] = oneLine.split('-')
                 seqOfTickers = seqOfTickers :+ (split.head, split.last)
@@ -32,10 +20,6 @@ class readTickers {
               }
             seqOfTickers
           } finally {
-            // Close open streams
-            // This will be executed if the file has been opened
-            // regardless of whether or not there were any exceptions.
-
             fileIn.close()
             linesIn.close()
             seqOfTickers
