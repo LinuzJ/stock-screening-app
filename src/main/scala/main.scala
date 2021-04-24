5bd7e0af96c4f9f023dc53f1a727d726d93c36d3
LinuzJ <linusjern00@gmail.com>
2021-04-24 17:24:16 +0300

    todo update

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,12 +5,11 @@ import scalafx.application.JFXApp
 object Main extends JFXApp {
 
   // TODO
-  // - Error handling for the data fetching
-  // - y scaling linechart
   // - Control Panel
   // - TESTING ????????????????
   //  KNOWN BUGS
   // - xaxis when chaning dates
+  // - datapane with emtyp sets error
 
   // read the tickers from setupfile
   val tickers = readTickers.fromFile("tickersConfig.txt")
